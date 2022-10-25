d4e721efabbfc9d680c6ed21344be3f687b2a6c4
BassCoder2808 <vedant.jolly@spit.ac.in>
2022-10-25 23:02:22 +0530

    Added AIDL SPIT Website

---
+++ Website/precache-manifest.615d2aa30e43dbac313d96b51845bc77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3817b35cb877fe1068393548f0da61e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9beb5bdd6c5b0f1864af",
+    "url": "/static/css/2.3248272f.chunk.css"
+  },
+  {
+    "revision": "a9d3c9380a2dad3328b9",
+    "url": "/static/css/main.d38f1380.chunk.css"
+  },
+  {
+    "revision": "9beb5bdd6c5b0f1864af",
+    "url": "/static/js/2.6cfd7ae7.chunk.js"
+  },
+  {
+    "revision": "549c9f469fb014d8eb1c89341c9856f2",
+    "url": "/static/js/2.6cfd7ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d3c9380a2dad3328b9",
+    "url": "/static/js/main.75cd8d05.chunk.js"
+  },
+  {
+    "revision": "7156accd511e957462f9",
+    "url": "/static/js/runtime-main.a668f7eb.js"
+  },
+  {
+    "revision": "965a7a1a7bd29d1565b5edeca7b4b9b7",
+    "url": "/static/media/icons_mobiscroll.965a7a1a.woff"
+  },
+  {
+    "revision": "c405c13ebacaadfc5def2aa6f9ae7b7d",
+    "url": "/static/media/icons_mobiscroll.c405c13e.ttf"
+  }
+]);
